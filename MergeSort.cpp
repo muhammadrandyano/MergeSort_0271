ea52f4c989e425fc6b00880537344bed36d04f1a
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-24 11:23:04 +0700

    menyalin sisa elemen dari sub array yg belum abis

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -57,4 +57,17 @@ void mergesort(int low,int hi)
         }
         k++;
     } 
+    while (j <= high)
+     {
+         B[k] = arr[j];
+         j++;
+         k++;
+     }
+ 
+     while (i <= mid)
+     {
+         B[k] = arr[i];
+         i++;
+         k++;
+     }
     
