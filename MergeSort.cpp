24c0c469d3c42a9eacf7802e445e8b030f4cff98
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-24 11:01:31 +0700

    menambahkan data rekursi

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -21,5 +21,17 @@ void input(){
     cout << "Masukkan element array" <<endl;
     cout << "======================" <<endl;
 
-    
+    for(int i=0;i<n;i++){
+        cout << "data ke-" << (i+1) << "=";
+        cin >> arr[i];
+    }
+
 }        
+
+void mergesort(int low,int hi)
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low+high)/ 2;
