fe10f65db747e0768686d0978cfea7e0ca8968a9
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-24 10:36:13 +0700

    prosedur input (membuat batasan 20 max 20 array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int arr[20],B[20];
+int n;
+
+void input(){
+    while (true){
+        cout << "Masukkan Jumlah data pada array = ";
+        cin >> n;
+
+        if (n <= 20 ){
+            break;
+        }
+        else{
+            cout << "\nArray maksimal 20 element.\n";
+        }
+    }
