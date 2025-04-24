4c8684d0f30de0e50fec4dabf26ea5a0921d1d52
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-24 11:26:27 +0700

    menyalin kembali dari array  bantu ke array utama

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -70,4 +70,21 @@ void mergesort(int low,int hi)
          i++;
          k++;
      }
-    
+     while (j <= high)
+     {
+         B[k] = arr[j];
+         j++;
+         k++;
+     }
+ 
+     while (i <= mid)
+     {
+         B[k] = arr[i];
+         i++;
+         k++;
+     }
+     for (int x = low; x <= high; x++)
+     {
+         arr[x] = B[x];
+     }
+
